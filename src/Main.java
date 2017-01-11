5d63239e356a86554688d6a15e915138da206be1
phoenix-oosd <s3595629@student.rmit.edu.au>
2017-01-11 13:40:05 +1100

    Now searches subdirectories of /logs.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -34,12 +35,13 @@ public class Main {
 		try {
 			scan = new Scanner(System.in);
 			// Files
-			String cwd = System.getProperty("user.dir");
-			new File(cwd + "/logs").mkdirs();
-			new File(cwd + "/graphs").mkdirs();
-			new File(cwd + "/tables").mkdirs();
-			File dir = new File(cwd + "/logs");
-			File[] logs = dir.listFiles();
+			File dir = new File("./logs");
+			dir.mkdir();
+			new File("./graphs").mkdirs();
+			new File("./tables").mkdirs();
+			List<File> logs = new ArrayList<File>();
+			recursiveFileSearch("./logs", logs);
+
 			// Menu loop
 			quitting = false;
 			while (!quitting) {
@@ -58,7 +60,7 @@ public class Main {
 				scan.nextLine();
 
 				// Parse ".evtc" files
-				if (logs.length > 0 && choice != 8) {
+				if (logs.size() > 0 && choice != 8) {
 					for (File log : logs) {
 						int status = parsing(choice, log);
 						if (status == -1) {
@@ -81,74 +83,68 @@ public class Main {
 
 	// Handle user choice
 	private static int parsing(int choice, File log) {
-		// Only parse ".evtc" files
-		String base = log.getName().split("\\.(?=[^\\.]+$)")[0];
-		String extn = log.getName().split("\\.(?=[^\\.]+$)")[1];
-		if (extn.equals("evtc")) {
-			if (is_in(choice, all_choices)) {
-				// Parse the log
-				System.out.println("Parsing " + base + "...");
-				Parse parser = new Parse(log);
-				Statistics stats = null;
+		if (is_in(choice, all_choices)) {
+			// Parse the log
+			String base = log.getName().split("\\.(?=[^\\.]+$)")[0];
+			System.out.println("Parsing " + base + "...");
+			Parse parser = new Parse(log);
+			Statistics stats = null;
+			try {
+				bossData b_data = parser.get_boss_data();
+				List<playerData> p_data = parser.get_player_data();
+				List<skillData> s_data = parser.get_skill_data();
+				List<combatData> c_data = parser.get_combat_data();
+				parser.fill_missing_data(b_data, p_data, s_data, c_data);
+				stats = new Statistics(b_data, p_data, s_data, c_data);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// A single choice
+			if (choice != 7) {
+				// Damage Related
+				if (is_in(choice, damage_choices)) {
+					stats.get_damage_logs();
+					if (choice == 1) {
+						System.out.println(stats.get_final_dps());
+					} else if (choice == 2) {
+						System.out.println(stats.get_phase_dps());
+					} else if (choice == 3) {
+						stats.get_total_damage_graph(base);
+					} else if (choice == 4) {
+						System.out.println(stats.get_combat_stats());
+					}
+				}
+				// Boon Related
+				else if (is_in(choice, boon_choices)) {
+					stats.get_boon_logs(boon_list);
+					if (choice == 5) {
+						System.out.println(stats.get_final_boons(boon_list));
+					} else if (choice == 6) {
+						System.out.println("Sorry, unimplemented.\n");
+						return -1;
+					}
+				}
+			}
+			// All choices
+			else {
+				// Write to file
+				stats.get_damage_logs();
+				stats.get_boon_logs(boon_list);
 				try {
-					bossData b_data = parser.get_boss_data();
-					List<playerData> p_data = parser.get_player_data();
-					List<skillData> s_data = parser.get_skill_data();
-					List<combatData> c_data = parser.get_combat_data();
-					parser.fill_missing_data(b_data, p_data, s_data, c_data);
-					stats = new Statistics(b_data, p_data, s_data, c_data);
+					File text_dump = new File(System.getProperty("user.dir") + "/tables/" + base + ".txt");
+					writeToFile(stats.get_final_dps() + stats.get_phase_dps() + stats.get_combat_stats()
+							+ stats.get_final_boons(boon_list), text_dump);
+					stats.get_total_damage_graph(base);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				// A single choice
-				if (choice != 7) {
-					// Damage Related
-					if (is_in(choice, damage_choices)) {
-						stats.get_damage_logs();
-						if (choice == 1) {
-							System.out.println(stats.get_final_dps());
-						} else if (choice == 2) {
-							System.out.println(stats.get_phase_dps());
-						} else if (choice == 3) {
-							stats.get_total_damage_graph(base);
-						} else if (choice == 4) {
-							System.out.println(stats.get_combat_stats());
-						}
-					}
-					// Boon Related
-					else if (is_in(choice, boon_choices)) {
-						stats.get_boon_logs(boon_list);
-						if (choice == 5) {
-							System.out.println(stats.get_final_boons(boon_list));
-						} else if (choice == 6) {
-							System.out.println("Sorry, unimplemented.\n");
-							return -1;
-						}
-					}
-				}
-				// All choices
-				else {
-					// Write to file
-					stats.get_damage_logs();
-					stats.get_boon_logs(boon_list);
-					try {
-						File text_dump = new File(System.getProperty("user.dir") + "/tables/" + base + ".txt");
-						writeToFile(stats.get_final_dps() + stats.get_phase_dps() + stats.get_combat_stats()
-								+ stats.get_final_boons(boon_list), text_dump);
-						stats.get_total_damage_graph(base);
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			} else {
-				System.out.println("Not a valid option. Try again.\n");
-				return -1;
 			}
-			System.out.println("Done!\n");
-			return 0;
+		} else {
+			System.out.println("Not a valid option. Try again.\n");
+			return -1;
 		}
-		System.out.println("Skipping " + base + "...\n");
-		return -2;
+		System.out.println("Done!\n");
+		return 0;
 	}
 
 	// Public Methods
@@ -162,6 +158,18 @@ public class Main {
 				PrintWriter writer = new PrintWriter(file, "UTF-8");) {
 			reader.lines().forEach(line -> writer.println(line));
 			writer.close();
+		}
+	}
+
+	private static void recursiveFileSearch(String dirName, List<File> files) {
+		File dir = new File(dirName);
+		File[] file_array = dir.listFiles();
+		for (File f : file_array) {
+			if (f.isFile() && f.toString().endsWith(".evtc")) {
+				files.add(f);
+			} else if (f.isDirectory()) {
+				recursiveFileSearch(f.getAbsolutePath(), files);
+			}
 		}
 	}
 
