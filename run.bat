4af58a2362ab1f1e182d6c6bbb2e781c4f54df31
phoenix-oosd <s3595629@student.rmit.edu.au>
2017-01-21 16:23:26 +1100

    Example options for running.

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-start "EVTC Log Parser" /MAX java -jar C:\Users\Chris\Desktop\parser\test.jar %1%
+start "EVTC Log Parser" /MAX java -jar C:\Users\User\Desktop\EVTC Log Parser\evtc_log_parser.jar %1% "12456"
