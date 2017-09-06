6866f4781db6795bcc8ff7f3da6cc4ba727d0e1c
phoenix-oosd <s3595629@student.rmit.edu.au>
2017-09-06 18:52:28 +1000

    New project files

---
+++ EVTC-Log-Parser/EVTC-Log-Parser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVTC_Log_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
